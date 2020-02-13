466c96f3b93ca8f39041041be6c01958a4fa6ffc
jpcuentas123 <jpcuentas123@gmail.com>
2020-02-13 15:14:24 -0500

    Blog desing.

--- src/component/Blog.js
+++ src/component/Blog.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Row, Col, Skeleton } from 'antd';
+import { Row, Col } from 'antd';
 import background from '../assets/img/BackgroundBlog.jpg'
+import Blogimg1 from '../assets/img/blog_1.png'
 
 class Blog extends React.Component {
     componentDidMount() {
@@ -18,13 +19,29 @@ class Blog extends React.Component {
                 </Col>
                 <Col span={24}>
                     <Row type="flex" justify="center" align="middle">
-                        <Col md={18}>
-                            <Row type="flex" justify="space-around" align="middle">
-                                <Skeleton active avatar title />
+                        <Col md={18} className="Blog-container">
+                            <Row type="flex" justify="space-around" align="middle" className="Blog-container--row">
+                                {/* <Skeleton active avatar title /> */}
                                 <Col md={5}>
-                                    {/* <img src={} alt="Imagen no disponible" /> */}
+                                    <img src={Blogimg1} alt="Imagen no disponible" />
                                 </Col>
-                                <Col md={10}></Col>
+                                <Col md={10}>
+                                    <Row>
+                                        <Col><h2>Viaje al amazonas</h2></Col>
+                                        <Col><small>07/01/2020</small></Col>
+                                        <Col>
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida ante magna, dignissim fermentum
+                                                lectus lobortis cursus. Vestibulum et elit vel lectus interdum varius. Etiam maximus odio erat, eget molestie
+                                                ex consequat vitae. Aliquam vehicula placerat erat at maximus. Morbi at aliquam ex, quis elementum nulla.
+                                                Maecenas rutrum odio mauris, in tempus nibh hendrerit eu. Aenean in sodales mauris, vitae elementum eros.
+                                                Curabitur convallis massa vel ipsum faucibus ultrices. Duis laoreet lectus felis, sit amet lobortis sem facilisis eu.
+                                                Etiam egestas placerat urna eu laoreet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
+                                                turpis egestas. Maecenas in tincidunt felis, sit amet dictum nulla. Etiam volutpat urna efficitur tincidunt pharetra.
+                                            </p>
+                                        </Col>
+                                    </Row>
+                                </Col>
                             </Row>
                         </Col>
                     </Row>
