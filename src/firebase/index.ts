facd1423fd6aed6d14c75589fbe79045b634bde5
Jorge <jorge.perez@meltstudio.co>
2022-10-06 21:56:05 -0500

    feat: add getFirebaseStorage

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,30 +1,41 @@
 // Import the functions you need from the SDKs you need
 import { FirebaseApp, initializeApp } from 'firebase/app';
 import { Firestore, getFirestore } from 'firebase/firestore';
+import { FirebaseStorage, getStorage } from 'firebase/storage';
 import { firebaseConfig } from './config';
 import SendEmail from './storage/sendEmail';
 
 class Storage {
-  private static firebaseApp: FirebaseApp | null = null;
+    private static firebaseApp: FirebaseApp | null = null;
 
-  public static emails: SendEmail;
+    public static emails: SendEmail;
 
-  static init() {
-    if (!this.firebaseApp) {
-      this.firebaseApp = initializeApp(firebaseConfig);
+    static init() {
+        if (!this.firebaseApp) {
+            this.firebaseApp = initializeApp(firebaseConfig);
 
-      this.emails = new SendEmail(this.firebaseApp);
-    }
-  }
-
-  public static getDb(): Firestore {
-    if (Storage.firebaseApp === null) {
-      throw new Error('firebase_not_initialized');
+            this.emails = new SendEmail(this.firebaseApp);
+        }
     }
 
-    const db = getFirestore(Storage.firebaseApp);
-    return db;
-  }
+    public static getDb(): Firestore {
+        if (Storage.firebaseApp === null) {
+            throw new Error('firebase_not_initialized');
+        }
+
+        const db = getFirestore(Storage.firebaseApp);
+        return db;
+    }
+
+    public static getFirebaseStorage(): FirebaseStorage {
+        if (Storage.firebaseApp === null) {
+            throw new Error('firebase_not_initialized');
+        }
+
+        const storage = getStorage(Storage.firebaseApp);
+
+        return storage;
+    }
 }
 
 export default Storage;
