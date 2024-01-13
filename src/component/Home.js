cb0b80faef850b51a7f01435413ccb24ff8866df
Jorge <jpcuentas123@gmail.com>
2024-01-13 09:13:45 -0500

    fix: rm blog section

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,30 +1,29 @@
-import React from 'react';
+import React from 'react'
 import About from './Home/About'
 import background from '../assets/img/Background1-min.png'
 import backgroundMb from '../assets/img/Background1-mb.jpg'
 
-import BlogPreview from './Home/blogPreview';
-import HomeExperiences from './Home/Experiences&Projects';
+import HomeExperiences from './Home/Experiences&Projects'
 
 class Home extends React.Component {
-    componentDidMount() {
-        if (window.screen.width > 800)
-            document.querySelector("body").style.backgroundImage = `url(${background})`;
-        else if (window.screen.width < 800)
-            document.querySelector("body").style.backgroundImage = `url(${backgroundMb})`;
-
-
-    };
-    render() {
-        return (
-            <div className="Home">
-                <About />
-                <HomeExperiences />
-                <BlogPreview />
-            </div>
-        )
-    }
-
+  componentDidMount() {
+    if (window.screen.width > 800)
+      document.querySelector(
+        'body'
+      ).style.backgroundImage = `url(${background})`
+    else if (window.screen.width < 800)
+      document.querySelector(
+        'body'
+      ).style.backgroundImage = `url(${backgroundMb})`
+  }
+  render() {
+    return (
+      <div className="Home">
+        <About />
+        <HomeExperiences />
+      </div>
+    )
+  }
 }
 
-export default Home;
+export default Home
